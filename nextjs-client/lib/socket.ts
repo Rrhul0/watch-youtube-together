466d8cb9e2510772824e59ff5f3375051713f342
Rrhul0 <rr025765@gmail.com>
2023-04-17 02:04:51 +0530

    fixed env variable for socket url as it is public env

--- nextjs-client/lib/socket.ts
+++ nextjs-client/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const socket = io(process.env.SOCKET_URL || 'localhost:3005')
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'localhost:3005')
 
 export default socket
